0ccebb049b7c1057285facc38bbb634082835ca8
Sulumer <i@cstdio.cn>
2018-11-22 21:15:00 +0800

    Integration existing code.

--- pages/index/index.js
+++ pages/index/index.js
@@ -161,22 +161,47 @@ Page({
   },
   onLoad: function (options) {
     // 生命周期函数--监听页面加载
-    
-    // wx.showLoading({
-    //   title: '',
+    wx.showLoading({
+      title: '加载中',
+    })
+    wx.getSetting({
+      success(res) {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function (res) {
+              console.log("success", res.userInfo)
+            }
+          })
+          setTimeout(function () {
+            wx.hideLoading()
+          }, 2)
+        }
+        else {
+          console.log("fail")
+          wx.navigateTo({
+            url: '/pages/login/login'
+          })
+        }
+      }
+    })
+    //请求获取用户信息
+    // wx.request({
+    //   url: 'test.php', //仅为示例，并非真实的接口地址
+    //   data: {
+    //     x: '',
+    //     y: ''
+    //   },
+    //   header: {
+    //     'content-type': 'application/json' // 默认值
+    //   },
+    //   success(res) {
+    //     console.log(res.data)
+    //     setTimeout(function () {
+    //       wx.hideLoading()
+    //     }, 2)
+    //   }
     // })
-    // var that = this
-    // console.log("1",that.flag)
-    // setTimeout(function () {
-    //   that.setData({
-    //     flag: false
-    //   })
-    // }, 2000)
-    // console.log(this.flag)
-    // if(this.flag){
-    //   wx.hideLoading()
-    // }
-    
   },
   onReady: function () {
     // 生命周期函数--监听页面初次渲染完成
