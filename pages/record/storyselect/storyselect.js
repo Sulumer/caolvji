05e0642a0592acd127911530b9a83faf815c0fcd
TTTIDE <530217259@qq.com>
2018-11-28 13:03:45 +0800

    update
    修改界面

--- pages/record/storyselect/storyselect.js
+++ pages/record/storyselect/storyselect.js
@@ -1,66 +1,81 @@
 // pages/find/storyselect/storyselect.js
+var app = getApp()
+var count = 0;
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    chooesVideo: '',    //上传视频地址
+    tipHide: false,
+    chooseTypeHide: true,
   },
 
   /**
-   * 生命周期函数--监听页面加载
-   */
+     * 生命周期函数--监听页面加载
+     */
   onLoad: function (options) {
+    console.log(options.status)
+  },
+  /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+  onReady: function (res) {
+    this.videoContext = wx.createVideoContext('prew_video');
+  },/**
+   * 上传图片
+   */
+  upphoto: function () {
+    let that = this
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        console.log(res)
+        var tempFilePaths = res.tempFilePaths
+        that.data.images = tempFilePaths
+        // 多图片
+        // that.data.urls = that.data.urls.concat(tempFilePaths)
+        // 单图片
+        that.data.urls = tempFilePaths[0]
+        that.setData({
+          images: tempFilePaths[0],
+          urls: that.data.urls
+        })
 
+      }
+    })
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 上传视频
    */
-  onReady: function () {
-
+  upmovie: function () {
+    let that = this
+    wx.chooseVideo({
+      sourceType: ['album', 'camera'],
+      maxDuration: 60,
+      camera: 'back',
+      success: function (res) {
+        that.setData({
+          chooesVideo: res.tempFilePath
+        })
+      }
+    })
   },
 
   /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+     * 全屏改变
+     */
+  bindVideoScreenChange: function (e) {
+    var status = e.detail.fullScreen;
+    var play = {
+      playVideo: false
+    }
+    if (status) {
+      play.playVideo = true;
+    } else {
+      this.videoContext.pause();
+    }
+    this.setData(play);
   }
+
 })
