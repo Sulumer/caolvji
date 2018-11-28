2e7a6d3c8530cfe6b7a9b9ac20c8baa1ee359a31
Sulumer <i@cstdio.cn>
2018-11-28 22:43:00 +0800

    Update some parts of index.

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,34 +1,38 @@
+var that;
+var app = getApp()
 Page({
   data: {
     flag: true,
     showUpload: true,
-    isPopping: false, //是否已经弹出
-    animPlus: {}, //旋转动画
-    animCollect: {}, //item位移,透明度
-    animTranspond: {}, //item位移,透明度
-    animInput: {}, //item位移,透明度
-    homeActionLeftDistance: '100rpx',
-    windowWidth: '',
-    markers: [{
-      iconPath: "/images/1542206356985.png",
-      id: 0,
-      latitude: 26.0510520000,
-      longitude: 119.1924740000,
-      width: 50,
-      height: 50,
-      callout: {
-        content: "福州大学 \n 三十一号楼 316 \n 到此一游  ",
-         fontSize: "16",
-        borderRadius: "10",
-        bgColor: "#CCFFFF",
-         padding: "5",
-         display: "byclick",
-        borderWidth: "15",
-        borderColor: "#3399FF",
+    isPopping: false,//是否已经弹出
+    animPlus: {},//旋转动画
+    animCollect: {},//item位移,透明度
+    animTranspond: {},//item位移,透明度
+    animInput: {},//item位移,透明度
+    homeActionLeftDistance:'100rpx',
+    windowWidth:'',
+    // markers: [{
+    //   iconPath: "/images/1542206356985.png",
+    //   id: 0,
+    //   latitude: 26.0510520000,
+    //   longitude: 119.1924740000,
+    //   width: 50,
+    //   height: 50,
+    //   callout: {
+    //     content: "福州大学 \n 三十一号楼 316 \n 到此一游  ",
+    //      fontSize: "16",
+    //     borderRadius: "10",
+    //     bgColor: "#CCFFFF",
+    //      padding: "5",
+    //      display: "byclick",
+    //     borderWidth: "15",
+    //     borderColor:"#3399FF",
 
-         
-      },
-    }],
+    //    },
+   
+
+    // }],
+    markers:[],
     polyline: [{
       points: [{
         longitude: 113.3245211,
@@ -54,7 +58,7 @@ Page({
     }],
   },
   //页面跳转
-  navi: function(e) {
+  navi: function(e){
     console.log(e.currentTarget.dataset.path)
     var path = e.currentTarget.dataset.path
     console.log(path)
@@ -63,13 +67,13 @@ Page({
     })
   },
   //点击弹出
-  ct: function(e) {
+  ct: function (e) {
     wx.navigateTo({
       url: '/pages/post/post'
     })
     console.log("头上文字被点击", e)
   },
-  plus: function() {
+  plus: function () {
     // this.setData({
     //   maskFlag: false,
     //   oilchooseFlag: true
@@ -88,22 +92,22 @@ Page({
       })
     }
   },
-  input: function() {
+  input: function () {
     console.log("input")
   },
-  transpond: function() {
+  transpond: function () {
     console.log("transpond")
   },
-  collect: function() {
+  collect: function () {
     console.log("collect")
   },
 
   //弹出动画
-  popp: function() {
+  popp: function () {
     //plus顺时针旋转
     var animationPlus = wx.createAnimation({
-      duration: 500, //动画持续时间
-      timingFunction: 'ease-out' //结束时减速
+      duration: 500,//动画持续时间
+      timingFunction: 'ease-out'//结束时减速
     })
     var animationcollect = wx.createAnimation({
       duration: 500,
@@ -117,10 +121,10 @@ Page({
       duration: 500,
       timingFunction: 'ease-out'
     })
-    animationPlus.rotateZ(180).step(); //在z轴旋转180度
-    animationcollect.translate(0, -20).rotateZ(360).opacity(1).step(); //opacity时透明度
-    animationTranspond.translate(0, -120).rotateZ(360).opacity(1).step();
-    animationInput.translate(0, -220).rotateZ(360).opacity(1).step();
+    animationPlus.rotateZ(180).step();//在z轴旋转180度
+    animationcollect.translate(12, -5).rotateZ(360).opacity(1).step();//opacity时透明度
+    animationTranspond.translate(12, -220).rotateZ(360).opacity(1).step();
+    animationInput.translate(10, -75).rotateZ(360).opacity(1).step();
     this.setData({
       animPlus: animationPlus.export(),
       animCollect: animationcollect.export(),
@@ -129,7 +133,7 @@ Page({
     })
   },
   //收回动画
-  takeback: function() {
+  takeback: function () {
     //plus逆时针旋转
     var animationPlus = wx.createAnimation({
       duration: 500,
@@ -158,7 +162,7 @@ Page({
       animInput: animationInput.export(),
     })
   },
-  onLoad: function(options) {
+  onLoad: function (options) {
     var that = this
     // 生命周期函数--监听页面加载
     wx.showLoading({
@@ -182,12 +186,12 @@ Page({
     wx.getStorage({
       key: 'S-TOKEN',
       success(resStorage) {
-        setTimeout(function() {
-          wx.hideLoading()
-        }, 2)
+        setTimeout(function () {
+            wx.hideLoading()
+          }, 2)
       },
-      fail(resStorage) {
-        wx.redirectTo({
+      fail(resStorage){
+        wx.navigateTo({
           url: '/pages/login/login'
         })
       }
@@ -196,25 +200,50 @@ Page({
     //   }
     // })
   },
-  onReady: function() {
+  onReady: function () {
     // 生命周期函数--监听页面初次渲染完成
   },
-  onShow: function() {
+  onShow: function () {
+    var that = this
     // 生命周期函数--监听页面显示
+    wx.getStorage({
+      key: 'S-TOKEN',
+      success(resmes) {
+        console.log(resmes.data)
+        wx.request({
+          url: app.globalData.Service + 'photo/map?', //仅为示例，并非真实的接口地址
+          method: "GET",
+          data: {
+          },
+          header: {
+            'content-type': 'application/json', // 默认值
+            "S-TOKEN": resmes.data
+          },
+          success(res) {
+            // console.log("index",res.data.data)
+            that.setData({
+              markers: res.data.data
+            })
+            console.log("markers", that.data.markers)
+          }
+        })
+      }
+    })
+   
   },
-  onHide: function() {
+  onHide: function () {
     // 生命周期函数--监听页面隐藏
   },
-  onUnload: function() {
+  onUnload: function () {
     // 生命周期函数--监听页面卸载
   },
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
     // 页面相关事件处理函数--监听用户下拉动作
   },
-  onReachBottom: function() {
+  onReachBottom: function () {
     // 页面上拉触底事件的处理函数
   },
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
     // 用户点击右上角分享
     return {
       title: 'title', // 分享标题
@@ -222,7 +251,7 @@ Page({
       path: 'path' // 分享路径
     }
   },
-  oilConfirm: function() {
+  oilConfirm: function () {
     this.setData({
       maskFlag: true,
       oilchooseFlag: false
@@ -237,5 +266,5 @@ Page({
   controltap(e) {
     console.log(e.controlId)
   },
-
+  
 })
