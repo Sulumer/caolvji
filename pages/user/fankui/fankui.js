73227558eb4f854af88f0b3e496872c795d82347
396406412 <396406412@qq.com>
2018-12-12 21:40:09 +0800

    improve fankui page

--- pages/user/fankui/fankui.js
+++ pages/user/fankui/fankui.js
@@ -5,13 +5,22 @@ Page({
    * 页面的初始数据
    */
   data: {
-    imgs:[],
+    imgs: [],
+    contentCount: 0,
+    left: 40,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  fankui:function(){
+  handleContentInput: function(e) {
+    var value = e.detail.value;
+
+    this.setData({
+      contentCount: value.length
+    });
+  },
+  fankui: function() {
     wx.redirectTo({
       url: '../tijiaochenggong/tijiaochenggong',
       success: function(res) {},
@@ -19,78 +28,81 @@ Page({
       complete: function(res) {},
     })
   },
-  xuanzezhaopian:function(){
-    var _this=this;
+  xuanzezhaopian: function() {
+    var that = this;
     wx.chooseImage({
       success: function(res) {
-        var tempFilePaths=res.tempFilePaths;
-        _this.setData({
-          imgs:tempFilePaths
+        var tempFilePaths = res.tempFilePaths;
+        var imgs = that.data.imgs.concat(tempFilePaths);
+         if (imgs.length > 2) imgs=imgs.pop;
+        that.setData({
+          imgs: imgs
         });
-        console.log(tempFilePaths)
+        console.log(imgs);
       },
     })
+    
   },
-  previewImg:function(e){
-    var current=e.target.dataset.src;
+  previewImg: function(e) {
+    var current = e.target.dataset.src;
     wx.previewImage({
       urls: this.data.imgs,
-      current:current,
-      success:function(e){
+      current: current,
+      success: function(e) {
         console.log('预览成功');
       }
     })
   },
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
