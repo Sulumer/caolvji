b2cc99212fa6b45c85c17da09f30e23dc26ab00b
KOGLinese <1076452761@qq.com>
2018-12-20 21:59:28 +0800

    integration all pages.hide some pages.

--- pages/user/user.js
+++ pages/user/user.js
@@ -4,17 +4,17 @@ const app = getApp()
 
 Page({
   data: {
-    name:'陈瀚霖',
-    country:"234",
-    province:"数量",
-    city:"33",
-    autoplay:false,
-    achieve:[
-      {
-        id:1,
-        url:'fankui/fankui',
-        src:'/images/achievement.png',
-        content:'说走就走'
+    nickName: '',
+    avatarUrl: '',
+    country: "1",
+    province: "5",
+    city: "33",
+    autoplay: false,
+    achieve: [{
+        id: 1,
+        url: 'fankui/fankui',
+        src: '/images/achievement.png',
+        content: '说走就走'
       },
       {
         id: 2,
@@ -29,8 +29,7 @@ Page({
         content: '说走就走'
       },
     ],
-    achieve1:[
-      {
+    achieve1: [{
         id: 4,
         url: 'fankui/fankui',
         src: '/images/achievement.png',
@@ -51,54 +50,82 @@ Page({
     ],
     //ctx=wx.createCanvasContext('firstCanvas')
   },
-  link:function(){
-      wx.navigateTo({
-        url: 'fankui/fankui',
-      })
+  link: function() {
+    wx.navigateTo({
+      url: 'fankui/fankui',
+    })
   },
-  btfankui:function(){
+  btfankui: function() {
     //console.log(res),
     wx.navigateTo({
       url: '/pages/user/fankui/fankui',
     })
   },
-  canvasIdErrorCallback: function (e) {
+  canvasIdErrorCallback: function(e) {
     console.error(e.detail.errMsg)
   },
-  onReady: function (e) {
+  onReady: function(e) {
     // 使用 wx.createContext 获取绘图上下文 context
-   /* var context = wx.createCanvasContext('firstCanvas')
+    /* var context = wx.createCanvasContext('firstCanvas')
 
-    context.setStrokeStyle('green')
-    context.setLineWidth(1)
-    context.moveTo(110, 50)
-    context.arc(80, 50, 30, 0, 2 * Math.PI,true)
-    context.moveTo(110, 130)
-    context.arc(80, 130, 30, 0, 2 * Math.PI, true)
-    context.moveTo(190, 50)
-    context.arc(160, 50, 30, 0, 2 * Math.PI, true)
-    context.moveTo(190, 130)
-    context.arc(160, 130, 30, 0, 2 * Math.PI, true)
-    context.moveTo(270, 50)
-    context.arc(240, 50, 30, 0, 2 * Math.PI, true)
-    context.moveTo(270, 130)
-    context.arc(240, 130, 30, 0, 2 * Math.PI, true)
-    context.stroke()
-    context.drawImage("../../picture/图片.png", 54, 35, 50, 28)
-    context.drawImage("../../picture/图片.png", 134, 35, 50, 28)
-    context.drawImage("../../picture/图片.png", 214, 35, 50, 28)
-    context.drawImage("../../picture/图片.png", 54, 115, 50, 28)
-    context.drawImage("../../picture/图片.png", 134, 115, 50, 28)
-    context.drawImage("../../picture/图片.png", 214, 115, 50, 28)
-    context.draw()*/
+     context.setStrokeStyle('green')
+     context.setLineWidth(1)
+     context.moveTo(110, 50)
+     context.arc(80, 50, 30, 0, 2 * Math.PI,true)
+     context.moveTo(110, 130)
+     context.arc(80, 130, 30, 0, 2 * Math.PI, true)
+     context.moveTo(190, 50)
+     context.arc(160, 50, 30, 0, 2 * Math.PI, true)
+     context.moveTo(190, 130)
+     context.arc(160, 130, 30, 0, 2 * Math.PI, true)
+     context.moveTo(270, 50)
+     context.arc(240, 50, 30, 0, 2 * Math.PI, true)
+     context.moveTo(270, 130)
+     context.arc(240, 130, 30, 0, 2 * Math.PI, true)
+     context.stroke()
+     context.drawImage("../../picture/图片.png", 54, 35, 50, 28)
+     context.drawImage("../../picture/图片.png", 134, 35, 50, 28)
+     context.drawImage("../../picture/图片.png", 214, 35, 50, 28)
+     context.drawImage("../../picture/图片.png", 54, 115, 50, 28)
+     context.drawImage("../../picture/图片.png", 134, 115, 50, 28)
+     context.drawImage("../../picture/图片.png", 214, 115, 50, 28)
+     context.draw()*/
   },
 
   //事件处理函数
   bindViewTap: function() {
-    
+
   },
-  onLoad: function () {
-   
+  onLoad: function() {
+    var that = this
+    console.log("that",that)
+    wx.getStorage({
+      key: 'S-TOKEN',
+      success(resStorage) {
+        console.log("S-TOKEN", resStorage.data)
+        wx.getStorage({
+          key: 'userinfo',
+          success: function(res) {
+            console.log("res",res.data)
+            that.setData({
+              avatarUrl: res.data.avatarUrl,
+              nickName: res.data.nickName
+            });
+            wx.request({
+              url: app.globalData.Service + 'user/me',
+              method: 'GET',
+              data: {},
+              header: {
+                'S-TOKEN': resStorage.data,
+                'content-type': 'application/json' // 默认值
+              },
+              success(result) {
+                console.log("result", result)
+              }
+            })
+          },
+        })
+      }
+    })
   }
-  
 })
