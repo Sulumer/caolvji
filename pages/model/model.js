20f0f7c0b1e0cbe71b7d9b3c5b8450044a906151
TTTIDE <530217259@qq.com>
2018-12-21 17:24:09 +0800

    update model.js

--- pages/model/model.js
+++ pages/model/model.js
@@ -1,4 +1,5 @@
 // pages/muban1/muban1.js
+var app = getApp()
 Page({
 
   /**
@@ -11,20 +12,20 @@ Page({
     id: '',
     title: '',
     url: '',
-    count: '',
+    count:'',
     story1: '在你的旅游生活中，你去过了N个城市，是一个旅游大魔王',
     story1_1: '在你的旅游生活中，你去过了',
     story1_2: '3',
     story1_3: '个城市，是一个旅游大魔王',
     story2: '你经常去往东方，是不是海岸线的风景深深地吸引了你',
     story3_1: '你热衷的口味成迷，但是你最近好像爱上了',
-    story3_2: '福州',
-    story3_3: ',我猜你定是在那儿找到了属于自己的羁绊。',
-    top: '我的故事',
+    story3_2:'福州',
+    story3_3:',我猜你定是在那儿找到了属于自己的羁绊。',
+    top:'我的故事',
   },
 
-
-
+  
+  
 
   /**
    * 生命周期函数--监听页面加载
@@ -39,11 +40,41 @@ Page({
     var url = '/images/model/model4.jpg'
     var title = '草履记'
     var back_img = "background-image: url(" + url + ")";
+    var startTime = 0
+    var endTime = 0
     console.log("za", back_img)
     that.setData({
       id: id,
       title: title,
       url: back_img
+    })
+    wx.getStorage({
+      key: 'S-TOKEN',
+      success(resStorage) {
+        console.log("S-TOKEN", resStorage.data)
+        wx.getStorage({
+          key: 'userinfo',
+          success: function (res) {
+            console.log("res", res.data)
+            that.setData({
+              avatarUrl: res.data.avatarUrl,
+              nickName: res.data.nickName
+            });
+            wx.request({
+              url: app.globalData.Service + 'photo/count?startTime=' + startTime + '&endTime=' + endTime,
+              method: 'GET',
+              data: {},
+              header: {
+                'S-TOKEN': resStorage.data,
+                'content-type': 'application/json' // 默认值
+              },
+              success(result) {
+                console.log("result", result)
+              }
+            })
+          },
+        })
+      }
     })
   },
 
@@ -181,5 +212,5 @@ Page({
   }
 
 
-
+  
 })
